2d0b6124dcdf04dbfcb4481285053401e158fc54
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-09-20 15:21:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,8 @@ class Transfer
 
   def execute_transaction
     if valid? && @status == "pending"
-      @sender.balance -= @amount
       @receiver.balance += @amount
+      @sender.balance -= @amount
       @status = "complete"
     else
       @status = "rejected"
