3ca245b2243f99a77118ed37ae3eca48139580f1
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-09-20 15:25:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,4 +25,13 @@ class Transfer
     end
   end
 
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
+  end
+
+
 end
